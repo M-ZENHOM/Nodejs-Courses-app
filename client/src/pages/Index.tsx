4f942a51c477649e3428a984abfdcc8e891be3c3
ZENHOM <zenhom.dev@gmail.com>
2023-11-14 19:21:43 +0200

    Remove testing pagination 🚀

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -32,7 +32,7 @@ function Index() {
       {isPending ?
         <div className="grid grid-cols-1 px-4 md:px-0 md:grid-cols-3 place-items-center gap-3 pb-20">
           {Array.from({ length: 6 }).map((_, i) => (
-            <Card key={i} className="p-6 space-y-4 w-full max-w-xs min-h-[150px]" >
+            <Card key={i} className="p-6 space-y-4 w-full max-w-md min-h-[130px]" >
               <Skeleton className="w-40 h-4" />
               <Skeleton className="w-20 h-4" />
             </Card>))}
@@ -40,7 +40,7 @@ function Index() {
         : <div className="grid grid-cols-1 px-4 md:px-0 md:grid-cols-3 place-items-center gap-3 pb-20">
           {data?.courses.map((course: course) => (
             <Link className="w-full" key={course._id} to={`/courses/${course._id}`}>
-              <Card className="p-6 space-y-4 w-full max-w-xs min-h-[150px]" >
+              <Card className="p-6 space-y-4 w-full max-w-md min-h-[130px]" >
                 <CardTitle>{course.title}</CardTitle>
                 <CardDescription>Price: {course.price}$</CardDescription>
               </Card>
@@ -49,27 +49,10 @@ function Index() {
         </div>}
       <div className="flex items-center space-x-4">
         <Link className={cn(buttonVariants({ variant: "outline" }),
-          { "pointer-events-none opacity-50": Number(data?.page) === 1 })}
+          { "pointer-events-none opacity-50": Number(data?.page) === 1 || Number.isNaN(Number(data?.pages)) })}
           to={`?page=${Number(data?.page) - 1}`} ><Icons.LeftArrow /></Link>
-        <div className="flex space-x-3">
-          {isPending ? (
-            <>
-              {Array.from({ length: 5 }).map((_, i) => (
-                <Skeleton key={i} className="bg-secondary w-10 h-10 leading-10  rounded-full text-center hover:bg-secondary/50" />
-              ))}
-            </>
-          ) : (
-            <>
-              {Array.from({ length: Number(data?.pages) }).map((_, i) => (
-                <Link key={i} className={cn("bg-secondary w-10 h-10 leading-10  rounded-full text-center hover:bg-secondary/50",
-                  { "bg-green-500": i + 1 === Number(data?.page) })}
-                  to={`?page=${i + 1}`}>{i + 1}</Link>
-              ))}
-            </>
-          )}
-        </div>
         <Link className={cn(buttonVariants({ variant: "outline" }),
-          { "pointer-events-none opacity-50": Number(data?.page) === Number(data?.pages) })}
+          { "pointer-events-none opacity-50": Number(data?.page) === Number(data?.pages) || Number.isNaN(Number(data?.pages)) })}
           to={`?page=${Number(data?.page) + 1}`} ><Icons.RightArrow /></Link>
       </div>
     </Wrapper >
